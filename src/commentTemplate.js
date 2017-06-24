2987e8516b4d5d60e3295fe28dd3d1f746969540
Jim Wethers <jim.wethers@me.com>
2017-06-24 01:58:20 +0100

    Update commentTemplate.js

--- src/commentTemplate.js
+++ src/commentTemplate.js
@@ -1,10 +1,10 @@
 export default ({ websiteName, formattedArticle, moreInfo, archiveUrl }) => [
-  `^(It looks like this is a link to ${websiteName}. Do you really need to read the full thing?)`,
+  `^(Do you really want to click a link to ${websiteName}?)`,
   '^(Here\'s a summary and a few images. More info below)',
+  archiveUrl ? `[^(View archived article at archive.is)](${archiveUrl})` : null,
   '----',
   formattedArticle,
   '----',
   moreInfo,
-  archiveUrl ? `[^(View archived page at archive.is)](${archiveUrl})` : null,
   '^(Problems or suggestions? Check out /r/dontclickbot. I\'m open source!)',
 ].join('\n\n');
